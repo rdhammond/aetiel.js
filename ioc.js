0842d933d445c18c77c3e859665c3f5df43675fa
Daniel Hammond <viztheman@gmail.com>
2016-07-12 23:54:43 -0400

    Set up some very basic E/T/L for input/output echoing.

--- ioc.js
+++ ioc.js
@@ -1,1 +1,17 @@
-module.exports = {};
+module.exports = {
+  streamTransform: require('stream-transform'),
+
+  Loader: require('./lib/loaders/Loader'),
+
+  extractors: [
+    ArrayExtractor: require('./lib/extractors/ArrayExtractor')
+  ],
+
+  transformers: [
+    LoopbackTransformer: require('./lib/transformers/LoopbackTransformer')
+  ],
+
+  loaders: [
+    ConsoleLoader: require('./lib/loaders/ConsoleLoader')
+  ]
+};
